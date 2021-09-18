8608af2a3332ce74d407bec17e980d26b177519a
Tathagata Raha <tathagata.raha@research.iiit.ac.in>
2021-09-18 23:43:29 +0000

    [Add] Emits

--- contracts/screens.sol
+++ contracts/screens.sol
@@ -4,16 +4,14 @@ contract NetflixScreen {
 
     // event NewZombie(uint zombieId, string name, uint dna);
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
-
     struct Item {
         string name;
         string description;
         uint price;
         uint seller;
         uint status;
-        string sold_to;
+        uint sold_to;
+        string content;
     }
 
     struct ViewAvlItem {
@@ -25,31 +23,35 @@ contract NetflixScreen {
     }
     Item[] public items;
 
+    event ViewItems(ViewAvlItem[] res);
+    event BuyItem(Item res);
+
     mapping (uint => uint[]) public ownerToItemIds;
     mapping (address => uint) UserID;
     uint users = 0;
-
-    function createItem(string memory _name, string memory description, uint price) public {
+    function createItem(string memory _name, string memory description, uint price, string memory content) public {
         if(UserID[msg.sender] == 0){
             UserID[msg.sender] = users + 1;
             users++;
         } 
-        uint id = items.push(Item(_name, description, price, UserID[msg.sender], 0, "None")) - 1;
-        ownerToItemIds[UserID[msg.sender]].push(id);
+        items.push(Item(_name, description, price, UserID[msg.sender], 0, 0, content));
     }
 
-    function viewItems() private view returns (uint) {
+    function viewItems() private {
         if(UserID[msg.sender] == 0){
             UserID[msg.sender] = users + 1;
             users++;
         } 
         ViewAvlItem[] memory res;
+        uint itr = 0;
         for (uint i =0; i < items.length; i += 1) {
             Item memory cur = items[i];
-            if (cur.status == 0)
-                res.push(ViewAvlItem(i, cur.name, cur.description, cur.price, cur.seller));
+            if (cur.status == 0){
+                res[itr] = ViewAvlItem(i, cur.name, cur.description, cur.price, cur.seller);
+                itr++;
+            }
         }
-        return res;
+        emit ViewItems(res);
     }
 
     function buyItem(uint id) public {
@@ -60,7 +62,7 @@ contract NetflixScreen {
         } 
         items[id].status = 1;
         items[id].sold_to = UserID[msg.sender];
-        return items[id];
+        emit BuyItem(items[id]);
     }
 
 }
